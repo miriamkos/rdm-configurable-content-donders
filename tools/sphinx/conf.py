89996a45138b663c982a807f14dce905ee905969
Hong Lee <h.lee@donders.ru.nl>
2018-11-20 14:12:09 +0100

    Update stylesheet and comments

--- tools/sphinx/conf.py
+++ tools/sphinx/conf.py
@@ -133,7 +133,7 @@ html_static_path = ['_static']
 # refs: http://alabaster.readthedocs.io/en/latest/installation.html#sidebars
 html_sidebars = {
     '**': [
-        'searchbox.html',
+        'searchbox.html', # this file is provided via local template
         'sidebar.html'    # this file is provided via local template
     ]
 }
